676c0d792103f32b01d3044ab7608543565acc36
Semyon Vasilkov <github@zimmnik.ru>
2022-02-24 00:57:15 +0300

    Added tmp structure, many variables moved to run.sh

--- run.sh
+++ run.sh
@@ -10,8 +10,15 @@ source wrtlab_functions
 
 export NAME="moswrt"
 export LIBVIRT_DIR_POOL_PATH="${HOME}/BOOT"
-export NET_INET="network=default,mac=52:54:00:55:90:01"
-export NET_HOME="network=nahtestnet"
+export ROUTER_INET_IP="192.168.122.100"
+
+export ROUTER_INET_MAC="52:54:00:55:90:00"
+export NET_INET="network=default,mac=${ROUTER_INET_MAC}"
+
+export HYPERVISOR_HOME_NET_IP="192.168.123.253"
+export ROUTER_HOME_NET_IP="192.168.123.254"
+export ROUTER_HOME_NET_NAME="${NAME}_homenet"
+export NET_HOME="network=${ROUTER_HOME_NET_NAME}"
 
 #NET_INET="type=direct,source=eno1,source_mode=bridge,mac=52:54:00:55:90:00"
 
