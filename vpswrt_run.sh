2da2b5a7c5a1dc96f9f235a4eff4acfd4a69de38
Semyon Vasilkov <github@zimmnik.ru>
2022-03-31 13:13:03 +0300

    Added http proxy support, fixed syntax bugs, renamed old variabled, some code moved to specific_virtnet_setup, removed needless code, enabled debug code, deploy moved to virtual networks

--- vpswrt_run.sh
+++ vpswrt_run.sh
@@ -7,32 +7,40 @@ set -o nounset
 set -o errexit
 
 export NAME="vpswrt"
-export LIBVIRT_DIR_POOL_PATH="/data/libvirt-boot"
+export LIBVIRT_DIR_POOL_PATH="/home/zorg/BOOT"
 
-export WRTLAB_WAN_IP="10.64.0.1"
+export WRTLAB_WAN_IP="192.168.122.102"
 export WRTLAB_WAN_MASK="255.255.255.0"
-export WRTLAB_WAN_GW="10.64.0.254"
-export WRTLAB_WAN_DNS="10.64.0.254"
+export WRTLAB_WAN_GW="192.168.122.1"
+export WRTLAB_WAN_DNS="192.168.122.1"
 export WRTLAB_WAN_MAC="52:54:00:55:90:03"
+export WRTLAB_WAN_NETWORKS=(--network "network=default,mac=${WRTLAB_WAN_MAC}")
 
-export WRTLAB_VPN_IP="192.168.88.254/24"
-export WRTLAB_VPN_NET="192.168.88.0/24"
+export WRTLAB_VPN_IP="192.168.99.254/24"
+export WRTLAB_VPN_NET="192.168.99.0/24"
 export WRTLAB_VPN_PORT="5151"
 export WRTLAB_VPN_PORT_HIDDEN="53"
 
-export WRTLAB_TEST_VPN_IP="192.168.88.253/32"
-export WRTLAB_TEST_VPN_GW="192.168.88.254"
+export WRTLAB_TEST_VPN_IP="192.168.99.253/32"
+export WRTLAB_TEST_VPN_GW="192.168.99.254"
+export WRTLAB_TEST_MAC="52:54:00:55:90:04"
+export WRTLAB_TEST_NETWORKS=(--network "network=default,mac=${WRTLAB_TEST_MAC}")
 
-export NET_INET="type=direct,source=eno2.2,source_mode=bridge"
-export NET_HOME="none"
-#export NET_INET="network=default,mac=${ROUTER_INET_MAC}"
+export WRTLAB_HOME_NETWORKS=(--network none)
+#export NET_INET="type=direct,source=eno1,source_mode=bridge"
+
+export WRTLAB_PROXY_ENABLED="true"
+export WRTLAB_PROXY_URL_LIBVIRT="http://${WRTLAB_WAN_GW}:3128"
+export WRTLAB_PROXY_URL_DOCKER="http://${HOSTNAME}:3128"
+export WRTLAB_PROXY_FEDORA_MAIN="http://ftp.halifax.rwth-aachen.de/fedora"
+export WRTLAB_PROXY_FEDORA_CODECS="http://codecs.fedoraproject.org/openh264"
 
 source cfg/default/bash_functions
 
 main () {
   build_image
-  #deploy
-  #test
+  deploy
+  test
 }
 
 main "$@"
