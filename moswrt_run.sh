27967d3094f951186e93e180022ddd6602e9382a
Semyon Vasilkov <github@zimmnik.ru>
2022-04-02 20:25:40 +0300

    Disabled debug networks, enabled router autostart, disabled debug ip reservation, removed useless code, disabled direct wg port access, disabled debug uhttpd access, changed forwarding rules, disabled test, disabled proxy

--- moswrt_run.sh
+++ moswrt_run.sh
@@ -6,42 +6,42 @@ set -o nounset
 set -o errexit
 
 NAME="moswrt"
-LIBVIRT_DIR_POOL_PATH="/home/zorg/BOOT"
+LIBVIRT_DIR_POOL_PATH="/data/libvirt-boot/"
 
 WRTLAB_WAN_IF="eth0"
-WRTLAB_WAN_IP="192.168.122.100"
+WRTLAB_WAN_IP="10.64.0.250"
 WRTLAB_WAN_MASK="255.255.255.0"
-WRTLAB_WAN_GW="192.168.122.1"
-WRTLAB_WAN_DNS="192.168.122.1"
+WRTLAB_WAN_GW="10.64.0.254"
+WRTLAB_WAN_DNS="62.112.106.130"
 WRTLAB_WAN_MAC="52:54:00:55:02:00"
 
-WRTLAB_WAN_NETWORKS=(--network "network=default,mac=${WRTLAB_WAN_MAC}")
+WRTLAB_WAN_NETWORKS=(--network "type=direct,source=eno1.2,source_mode=bridge,mac=${WRTLAB_WAN_MAC}")
+#WRTLAB_WAN_NETWORKS=(--network "network=default,mac=${WRTLAB_WAN_MAC}")
 
 WRTLAB_MNGT_IF="eth1"
-WRTLAB_MNGT_IP="192.168.123.254"
+WRTLAB_MNGT_IP="192.168.1.254"
 WRTLAB_MNGT_MASK_LONG="255.255.255.0"
 WRTLAB_MNGT_NAME="${NAME}_mngt"
 WRTLAB_MNGT_IP_HOST="192.168.123.253"
 
 WRTLAB_HOME_IF="eth2"
-WRTLAB_HOME_IP="192.168.124.254"
+WRTLAB_HOME_IP="192.168.4.254"
 WRTLAB_HOME_MASK_LONG="255.255.255.0"
 WRTLAB_HOME_NAME="${NAME}_home"
 WRTLAB_HOME_IP_HOST="192.168.124.253"
 
 WRTLAB_DMZ_IF="eth3"
-WRTLAB_DMZ_IP="192.168.125.254"
+WRTLAB_DMZ_IP="192.168.3.254"
 WRTLAB_DMZ_MASK_LONG="255.255.255.0"
 WRTLAB_DMZ_NAME="${NAME}_dmz"
 WRTLAB_DMZ_IP_HOST="192.168.125.253"
 
-WRTLAB_LOCAL_NETWORKS=(--network "network=${WRTLAB_MNGT_NAME}" --network "network=${WRTLAB_HOME_NAME}" --network "network=${WRTLAB_DMZ_NAME}")
-#export WRTLAB_LOCAL_NETWORKS=(--network "network=default,mac=${WRTLAB_WAN_MAC}")
-#export NET_INET="type=direct,source=eno1,source_mode=bridge"
+WRTLAB_LOCAL_NETWORKS=(--network type=direct,source=eno1,source_mode=bridge --network type=direct,source=eno1.4,source_mode=bridge --network type=direct,source=eno1.3,source_mode=bridge)
+#WRTLAB_LOCAL_NETWORKS=(--network "network=${WRTLAB_MNGT_NAME}" --network "network=${WRTLAB_HOME_NAME}" --network "network=${WRTLAB_DMZ_NAME}")
 
-WRTLAB_VPN_IP="192.168.88.254"
+WRTLAB_VPN_IP="192.168.5.254"
 WRTLAB_VPN_MASK_SHORT="24"
-WRTLAB_VPN_NET="192.168.88.0/24"
+WRTLAB_VPN_NET="192.168.5.0/24"
 WRTLAB_VPN_PORT="5151"
 WRTLAB_VPN_PORT_HIDDEN="53"
 
@@ -61,7 +61,7 @@ WRTLAB_TEST_HOME_MAC="52:54:00:55:02:03"
 WRTLAB_TEST_DMZ_IP="192.168.125.200"
 WRTLAB_TEST_DMZ_MAC="52:54:00:55:02:04"
 
-WRTLAB_PROXY_ENABLED="true"
+WRTLAB_PROXY_ENABLED="false"
 WRTLAB_PROXY_URL_LIBVIRT="http://${WRTLAB_WAN_GW}:3128"
 WRTLAB_PROXY_URL_DOCKER="http://${HOSTNAME}:3128"
 WRTLAB_PROXY_FEDORA_MAIN="http://ftp.halifax.rwth-aachen.de/fedora"
@@ -72,7 +72,7 @@ source cfg/default/bash_functions
 main () {
   build_image
   deploy
-  test
+  #test
 }
 
 main "$@"
