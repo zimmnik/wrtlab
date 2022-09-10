137c84105ad570ffb185abafa407a13933dd280f
Semyon Vasilkov <svasilkov@inno.tech>
2022-09-10 20:41:44 +0300

    fixed proxy env error, moved dockerfiele

--- run.sh
+++ run.sh
@@ -10,10 +10,6 @@ WRTLAB_VERSION="22.03.0"
 LIBVIRT_DIR_POOL_PATH="/home/zorg/BOOT"
 
 WRTLAB_PROXY_ENABLED="true"
-WRTLAB_PROXY_URL_LIBVIRT="http://${WRTLAB_WAN_GW}:3128"
-WRTLAB_PROXY_URL_DOCKER="http://${HOSTNAME}:3128"
-WRTLAB_PROXY_FEDORA_MAIN="http://fedora.mirrorservice.org/fedora"
-WRTLAB_PROXY_FEDORA_CODECS="http://codecs.fedoraproject.org/openh264"
 
 source profile/common/bash_functions
 
